420cfb6c518b26163023368fb9c4164911e9e132
oumaimasb <96146890+oumaimasb@users.noreply.github.com>
2022-04-07 23:38:43 +0200

    done

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://Oumaimasb:Oumaima123<3@cluster0.amgeq.mongodb.net/mern?retryWrites=true&w=majority",
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.amgeq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
   )
   .then(() => {
